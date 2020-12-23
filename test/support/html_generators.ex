b5f63787411dde1391aacc7e3b3873b4723771af
Javier Garea <javigarea@gmail.com>
2020-12-23 16:26:37 +0100

    style: deleted aux_ prefix on private functions

--- test/support/html_generators.ex
+++ test/support/html_generators.ex
@@ -111,16 +111,16 @@ defmodule HTMLGenerators do
   defp insensitive_case_string(string) do
     string
     |> String.split("", trim: true)
-    |> aux_insensitive_case_string([])
+    |> insensitive_case_string([])
     |> Enum.join("")
   end
 
-  defp aux_insensitive_case_string([], result), do: result
+  defp insensitive_case_string([], result), do: result
 
-  defp aux_insensitive_case_string([h | t], result) do
-    aux_insensitive_case_string(t, result ++ [aux_insensitive_fun(Enum.random(0..1), h)])
+  defp insensitive_case_string([h | t], result) do
+    insensitive_case_string(t, result ++ [insensitive_fun(Enum.random(0..1), h)])
   end
 
-  defp aux_insensitive_fun(0, string), do: String.downcase(string)
-  defp aux_insensitive_fun(1, string), do: String.upcase(string)
+  defp insensitive_fun(0, string), do: String.downcase(string)
+  defp insensitive_fun(1, string), do: String.upcase(string)
 end
