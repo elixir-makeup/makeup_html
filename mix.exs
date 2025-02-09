412a27ac1dc44d9bef54d35072fad3a36ba7f7e9
José Valim <jose.valim@dashbit.co>
2025-02-09 16:54:05 +0100

    Release v0.2.0 with pygments consistent rendering, closes #3

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule MakeupHTML.Mixfile do
   def project do
     [
       app: :makeup_html,
-      version: "0.1.2",
+      version: "0.2.0",
       elixir: "~> 1.10",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
